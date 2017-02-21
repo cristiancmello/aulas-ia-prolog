80c1d6b16a440857aa70c124cf487cb9f7fc3cb4
Cristian Mello <cristianc.mello@gmail.com>
2017-02-20 22:43:57 -0300

    Add files via upload

--- aula-02-20/aula-02-20-secao-4-14-unificacao.pl
+++ aula-02-20/aula-02-20-secao-4-14-unificacao.pl
@@ -1,55 +1,56 @@
-/* 4.14 UNIFICA√á√ÉO */
-/* H√° os seguintes crit√©rios:
+/* 4.14 UNIFICA«√O */
+/* H· os seguintes critÈrios:
  *  IGUALDADE:
- *    a) constantes s√£o iguais => escritas do mesmo jeito. Ex.: n√°dia e
- *    n√°dia s√£o iguais
+ *    a) constantes s„o iguais => escritas do mesmo jeito. Ex.: n·dia e
+ *    n·dia s„o iguais
  *
- *    b) 2 n√∫meros s√£o iguais => representam o mesmo valor. Ex.: 34 √© igual a 34
+ *    b) 2 n˙meros s„o iguais => representam o mesmo valor. Ex.: 34 È igual a 34
  *
- *    c) vari√°vel comparada com outro objeto => ela se torna igual a
- *    este objeto. Dizemos que a vari√°vel est√° representando o objeto.
- *        Vari√°veis livres: s√£o aquelas que n√£o est√£o representando
+ *    c) vari·vel comparada com outro objeto => ela se torna igual a
+ *    este objeto. Dizemos que a vari·vel est· representando o objeto.
+ *        Vari·veis livres: s„o aquelas que n„o est„o representando
  *        objeto algum.
  *
- *    d) 2 estruturas s√£o iguais => devem possui mesmo funtor e mesma
+ *    d) 2 estruturas s„o iguais => devem possui mesmo funtor e mesma
  *    aridade. Argumentos devem ser iguais entre ambas as partes.
  *
- *  UNIFICA√á√ÉO:
- *    √â basicamente uma compara√ß√£o na qual as vari√°veis podem se tornar
+ *  UNIFICA«√O:
+ *    … basicamente uma comparaÁ„o na qual as vari·veis podem se tornar
  *    iguais a qualquer objeto.
  *
- *    IMPORTANTE! Uma vez que a vari√°vel tornou-se igual a um objeto,
- *    ela n√£o pode mais ser igual a outro diferente do primeiro.
+ *    IMPORTANTE! Uma vez que a vari·vel tornou-se igual a um objeto,
+ *    ela n„o pode mais ser igual a outro diferente do primeiro.
  */
 
 % Predicado unifica os 2 argumentos.
-% A estrutura da esquerda √© UNIFICADA com a da direita.
+% A estrutura da esquerda È UNIFICADA com a da direita.
 % ama(X, hadar) = ama(nadia, hadar).
 % X = nadia.
 
-/* COMPARA√á√ÉO DE DOIS OBJETOS */
-/* O predicado da compara√ß√£o considera que ambos os lados representam o mesmo objeto.
- *  Ex.:  L√™-se -> 'X √© equivalente a Y'
+/* COMPARA«√O DE DOIS OBJETOS */
+/* O predicado da comparaÁ„o considera que ambos os lados representam o mesmo objeto.
+ *  Ex.:  LÍ-se -> 'X È equivalente a Y'
  *  ?- X == Y.
- *  false. => A resposta foi falsa porque X n√£o representa coisa alguma.
- *  No entanto, se X = Y, a resposta ser√° 'true', pois X se tornar√° igual a Y
+ *  false. => A resposta foi falsa porque X n„o representa coisa alguma.
+ *  No entanto, se X = Y, a resposta ser· 'true', pois X se tornar· igual a Y
  *
- *  Ex. 1: X passou a representar 'hadar' na primeira unifica√ß√£o e como
- *  'hadar' √© diferente de 'silvestre', a segunda unifica√ß√£o falha
+ *  Ex. 1: X passou a representar 'hadar' na primeira unificaÁ„o e como
+ *  'hadar' È diferente de 'silvestre', a segunda unificaÁ„o falha
  *
  *  ?- X=hadar, X=silvestre
  *  false.
  */
 
-/* COMPARA√á√ÉO DE CONSTANTES */
+/* COMPARA«√O DE CONSTANTES */
 /*
  * IMPORTANTE!
  * ?- hadar=hadar, hadar==hadar.
- * true. => Quando se compara constantes, '=' e '==' S√ÉO EQUIVALENTES.
+ * true. => Quando se compara constantes, '=' e '==' S√O EQUIVALENTES.
  */
 
-/* COMPARA√á√ÉO ENTRE VARI√ÅVEIS */
+/* COMPARA«√O ENTRE VARI¡VEIS */
 /*
  * ?- R=piter, S=piter, S==R, S=R.
  * R=S, S=piter.
  */
+
