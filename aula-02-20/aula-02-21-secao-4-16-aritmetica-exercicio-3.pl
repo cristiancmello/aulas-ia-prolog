756d7399fa837d7c70efd7086d675b388e6cce73
Cristian Mello <cristianc.mello@gmail.com>
2017-02-23 00:18:55 -0300

    Delete aula-02-21-secao-4-16-aritmetica-exercicio-3.pl

--- aula-02-20/aula-02-21-secao-4-16-aritmetica-exercicio-3.pl
+++
@@ -1,5 +0,0 @@
-bhaskara(A, B, C):-
-    X1 is ((-1) * B + sqrt(B^2 - (4*A*C))) / 2 * A,
-    X2 is ((-1) * B - sqrt(B^2 - (4*A*C))) / 2 * A,
-    write('X1='),write(X1),nl,
-    write('X2='),write(X2).
