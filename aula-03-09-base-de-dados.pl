eb2c26b7ab25476240c0691771e1fd88202c3e97
Cristian Mello <cristianc.mello@gmail.com>
2017-03-14 10:15:43 -0300

    Rename aula-03-09-base-de-dados.pl to aula-03-14/aula-03-09-base-de-dados.pl

--- aula-03-09-base-de-dados.pl
+++
@@ -1,4 +0,0 @@
-:- dynamic(homem/1).
-
-% Obs.: A seção de base de dados será pedida na LISTA 3
-% Estudar: dynamic, abolish
---
+++ aula-03-14/aula-03-09-base-de-dados.pl
@@ -0,0 +1,4 @@
+:- dynamic(homem/1).
+
+% Obs.: A seção de base de dados será pedida na LISTA 3
+% Estudar: dynamic, abolish
