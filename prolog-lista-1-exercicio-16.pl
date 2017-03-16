19b34fb84453c18a25517ab277337643875e35db
Cristian Mello <cristianc.mello@gmail.com>
2017-03-16 07:41:49 -0300

    Rename exercicio-16.pl to prolog-lista-1-exercicio-16.pl

--- prolog-lista-1-exercicio-16.pl
+++ prolog-lista-1-exercicio-16.pl
@@ -2,7 +2,7 @@
 % Base: X^0 = 1
 % Positivo: potencia(2, 2, R) => 2^2 = (2*(2^1)) = 2*(2*(2^0)) = 4
 
-potencia(_, 0, 1):- !. % potencia de qualquer coisa dá 1
+potencia(_, 0, 1):- !. % potencia de qualquer coisa dÃ¡ 1
 potencia(B, E, RN):-
 	E > 0, !,
 	E1 is E-1,
