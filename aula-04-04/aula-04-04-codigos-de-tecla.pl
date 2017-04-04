42a44657592005fc5f2d5b887401f80c6cc30143
Cristian Mello <cristianc.mello@gmail.com>
2017-04-04 10:34:51 -0300

    Rename aula-04-04-codigos-de-tecla.pl to aula-04-04/aula-04-04-codigos-de-tecla.pl

--- aula-04-04/aula-04-04-codigos-de-tecla.pl
+++ aula-04-04/aula-04-04-codigos-de-tecla.pl
@@ -1,5 +1,5 @@
 sx(S):- repeat,
-	writeln('Qual é o seu sexo?'),
+	writeln('Qual Ã© o seu sexo?'),
 	writeln('Aperte f para responder feminino'),
 	writeln('ou m para masculino: '),
 	get0(Tecla),
@@ -10,13 +10,13 @@ sexo(109, masculino).
 
 sabe_programar(E):-
 	repeat,nl,
-	write('Você sabe PROLOG? '),nl,
+	write('VocÃª sabe PROLOG? '),nl,
 	write('Aperte s para responder sim.'),nl,
-	write('Aperte n para responder não.'),nl,
+	write('Aperte n para responder nÃ£o.'),nl,
 	get0(Tecla),sn(Tecla,E).
 
 sn(115,sim):- !.
-sn(110,não).
+sn(110,nÃ£o).
 
 
 
